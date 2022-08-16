55d340adb8281db0eff7ceddf99f055ea3781238
omarhammami1992 <omar.hammami.o.h@gmail.com>
2022-08-16 16:30:57 +0200

    integrate cucumber

---
+++ back/src/test/java/com/soat/back/RunCucumberTest.java
@@ -0,0 +1,16 @@
+package com.soat.back;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("com/soat/back")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.soat.back")
+public class RunCucumberTest {
+}

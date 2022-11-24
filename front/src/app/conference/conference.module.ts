102e58c2fb9a32bed78b687271c69570bf1341f5
Ahd Bk <ahd.benkheder@gmail.com>
2022-11-24 22:33:17 +0100

    fix typo

--- front/src/app/conference/conference.module.ts
+++ front/src/app/conference/conference.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConferenceFormComponent } from './component/conference-form/conference-form.component';
-import {ConferenceRootingModule} from "./conference-rooting.module";
+import {ConferenceRoutingModule} from "./conference-routing.module";
 import { CreateConferencePageComponent } from './pages/create-conference-page/create-conference-page.component';
 import { HomeConferencePageComponent } from './pages/home-conference-page/home-conference-page.component';
 
@@ -14,7 +14,7 @@ import { HomeConferencePageComponent } from './pages/home-conference-page/home-c
   ],
   imports: [
     CommonModule,
-    ConferenceRootingModule
+    ConferenceRoutingModule
   ]
 })
 export class ConferenceModule { }

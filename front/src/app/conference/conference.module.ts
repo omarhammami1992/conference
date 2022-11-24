eb9e66a7e14e21b64e600d40595e72da61bcddce
omarhammami1992 <omar.hammami.o.h@gmail.com>
2022-11-24 17:16:44 +0100

    update rooting

--- front/src/app/conference/conference.module.ts
+++ front/src/app/conference/conference.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CreateConferencePageComponent } from './create-conference-page/create-conference-page.component';
 import { ConferenceFormComponent } from './component/conference-form/conference-form.component';
 import {ConferenceRootingModule} from "./conference-rooting.module";
-
+import {ConferenceComponent} from "./conference.component";
 
 
 @NgModule({
   declarations: [
+    ConferenceComponent,
     CreateConferencePageComponent,
     ConferenceFormComponent
   ],
--- front/src/app/conference/create-conference-page/create-conference-page.component.spec.ts
+++ front/src/app/conference/create-conference-page/create-conference-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateConferencePageComponent } from './create-conference-page.component';
+import {By} from "@angular/platform-browser";
 
 describe('CreateConferencePageComponent', () => {
   let component: CreateConferencePageComponent;
@@ -22,4 +23,23 @@ describe('CreateConferencePageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('template', () => {
+    it('should contain Conference Form', () => {
+      //When
+      const appCreateConferenceComponent = fixture
+        .debugElement
+        .query(By.css('app-conference-form'));
+
+      //Then
+      expect(appCreateConferenceComponent).toBeTruthy()
+    });
+
+
+
+
+
+  });
+
+
 });

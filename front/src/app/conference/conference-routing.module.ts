5280147d291d3f60e33b3be9fa87f7357cfa68ae
Ahd Bk <ahd.benkheder@gmail.com>
2022-11-24 22:33:17 +0100

    fix typo

--- front/src/app/conference/conference-routing.module.ts
+++ front/src/app/conference/conference-routing.module.ts
@@ -13,5 +13,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ConferenceRootingModule {
+export class ConferenceRoutingModule {
 }

2458398f2c4812d639cb0dcf8ea3f5102936d3fd
Masataka ISHII <masataka.ishii@soat.fr>
2023-04-03 13:20:43 +0200

    fix conference jpa adapter it test

--- back/src/test/java/com/soat/back/conference/command/infrastructure/ConferenceJpaAdapterITest.java
+++ back/src/test/java/com/soat/back/conference/command/infrastructure/ConferenceJpaAdapterITest.java
@@ -1,7 +1,10 @@
 package com.soat.back.conference.command.infrastructure;
 
 import com.soat.back.common.infrastructure.JpaConferenceRepository;
-import com.soat.back.conference.command.domain.*;
+import com.soat.back.conference.command.domain.Conference;
+import com.soat.back.conference.command.domain.InvalidAttendingDaysException;
+import com.soat.back.conference.command.domain.PriceAttendingDay;
+import com.soat.back.conference.command.domain.PriceAttendingDays;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,19 +24,17 @@ class ConferenceJpaAdapterITest {
     JpaConferenceRepository jpaConferenceRepository;
 
     @Test
-    void save_data() throws InvalidAttendingDaysException, InvalidIntervalException, InvalidPricesException {
-        Conference conference = Conference.create(
-                "conference aaa",
-                "link to aaa",
-                100f,
-                LocalDate.of(2023, 5, 15),
-                LocalDate.of(2023, 5, 19),
-                "city",
-                "country",
-                PriceRanges.createEmpty(),
-                PriceGroup.createEmpty(),
-                PriceAttendingDays.create(List.of(new PriceAttendingDay(100f, 1f)))
-        );
+    void save_data() throws InvalidAttendingDaysException {
+        Conference conference = Conference.builder()
+                .name("conference aaa")
+                .link("link to aaa")
+                .price(100f)
+                .startDate(LocalDate.of(2023, 5, 15))
+                .endDate(LocalDate.of(2023, 5, 19))
+                .city("city")
+                .country("country")
+                .priceAttendingDays(PriceAttendingDays.create(List.of(new PriceAttendingDay(100f, 1f))))
+                .build();
         var id = conferenceAdapter.save(conference);
         assertThat(id).isNotNull();
     }
